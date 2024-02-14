4821236e75c3ddebff0fa1ab7a87085c3eafe885
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2024-02-14 15:12:21 -0700

    rename to aezpz missed files

--- download_globals.py
+++ download_globals.py
@@ -1,9 +1,9 @@
-import adobe_xdm
+import aezpz
 import pandas as pd
 import json
 from pathlib import Path
 
-api = adobe_xdm.load_config('auth.json')
+api = aezpz.load_config('auth.json')
 global_resource_types = {}
 
 def add_resource_type(resources, resource_type):
@@ -16,5 +16,5 @@ add_resource_type(api.field_groups.findall(container='global'), 'fieldgroups')
 add_resource_type(api.data_types.findall(container='global'), 'datatypes')
 add_resource_type(api.behaviors.findall(container='global'), 'behaviors')
 
-f = Path(__file__).absolute().parent / 'adobe_xdm/globals.json'
+f = Path(__file__).absolute().parent / 'aezpz/globals.json'
 json.dump(global_resource_types, f.open('w'), indent='  ')
